16dd1882e3f3cf2f3d4f79587e795ffca4c0321e
Lautaro Riveiro <lautaroriveiro91@gmail.com>
2017-05-02 01:13:10 -0300

    Página de prueba
    Falta el envío de los mensajes al Firebase

---
+++ script.js
@@ -0,0 +1,15 @@
+$('#port1').click(function(){
+	window.location.href = "http://www.namastesanpedro.com.ar";
+})
+$('#port2').click(function(){
+	window.location.href = "http://www.lautaroriveiro.esy.es";
+})
+$('#port3').click(function(){
+	window.location.href = "https://tpfinalionic2016.firebaseapp.com/";
+})
+$('#descargaImg').click(function(){
+	window.location.href = "https://www.google.com.ar/";
+})
+function Enviar(){
+	alert("Enviar al Firebase")
+}
